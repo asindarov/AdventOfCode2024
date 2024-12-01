fcf9a7e41f8eadc40fc8b7b3195fe58ade33442f
asindarov <aosindarov@gmail.com>
2024-12-01 22:10:31 +0500

    Some polishing on code

--- Day1/Solution.fsx
+++ Day1/Solution.fsx
@@ -7,19 +7,29 @@
 3   3
 """
 
-let twoLists = input.Trim().Split('\n') |> List.ofArray |> List.map(fun x -> [x.Split(' ')[0] |> int; x.Split(' ')[3] |> int])
+let twoLists = input
+                   .Trim()
+                   .Split('\n')
+               |> List.ofArray
+               |> List.map(fun x ->
+                   [x.Split(' ')[0] |> int
+                    ; x.Split(' ')[3] |> int])
 
-let firstList = twoLists |> List.collect (fun x -> [x.[0]]) |> List.sort
-let secondList = twoLists |> List.collect (fun x -> [x.[1]]) |> List.sort
+let firstList = twoLists
+                |> List.collect (fun x -> [x.[0]])
+                |> List.sort
+let secondList = twoLists
+                 |> List.collect (fun x -> [x.[1]])
+                 |> List.sort
 
-let safeMinus f s =
+let (--) f s =
     match f > s with
         | true -> f - s
         | false ->  s - f
 
 let resultForPart1 =
     List.zip firstList secondList
-    |> List.sumBy (fun (f, s) -> safeMinus f s)
+    |> List.sumBy (fun (f, s) -> f -- s)
 
 let resultForPart2 =
     firstList
@@ -27,4 +37,3 @@ let resultForPart2 =
             fle, List.filter
                      (fun sle -> sle = fle) secondList |> List.length)
         |> List.sumBy (fun (f, s) -> f * s)
-        
